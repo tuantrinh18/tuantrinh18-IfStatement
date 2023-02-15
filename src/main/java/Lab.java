86e05f7a8dc4fb04173a709a989dcbbdbc7e709e
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-15 20:19:58 +0000

    LabDay3 completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,13 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+        if(bool == true)
+        {
+            return 0;
+        }
+        else{
+            return 1;
+        }
+        
     }
 }
